79a3f7d1fd050ccb9e8b71a896067bb4bc0e5216
Belenov Nurdaulet <94783522+BelenovNurdaulet@users.noreply.github.com>
2024-06-27 05:46:26 +0600

    Создал контроллеры , и представления

---
+++ VacationApplication.DAL/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VacationApplication.Portal.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VacationApplication.DAL/Controllers/VacationRequestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VacationApplication.Portal.Controllers
+{
+    public class VacationRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ApprovedRequests()
+        {
+            return View();
+        }
+    }
+}
